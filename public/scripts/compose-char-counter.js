b42241bfd8fce580b66f42138f44a98bc6dfebd8
Jaskaran Singh <s.jaskaran4@gmail.com>
2018-02-05 13:10:33 +0000

    comitting changes to submit project

--- public/scripts/compose-char-counter.js
+++ public/scripts/compose-char-counter.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('textarea').keyup(function(){
-    var inputLength = $(this).val().length;
-    var remain = 140 - inputLength;
-    $('.counter').html(remain);
-    if(remain >= 0){
+    var userInput = $(this).val().length;
+    var charLimit = 140 - userInput;
+    $('.counter').html(charLimit);
+    if(charLimit >= 0){
       $('.counter').css('color','black');
     } else {
       $('.counter').css('color','red');
